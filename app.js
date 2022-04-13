2d954c773090afecdebf789e5a857c00fe5ddb71
Alexis Jesus Peralta Holyoak <peraltaholyoak.aj@gmail.com>
2022-04-13 17:50:27 -0500

    added privacy policy for anemiar

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = express();
 app.use(express.static("public"));
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+"/index.html"));
+    res.sendFile(__dirname+"/index.html");
 });
+app.get("/anemiar", function(req,res){
+    res.sendFile(__dirname+"/public/privacy.html");
+});
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
